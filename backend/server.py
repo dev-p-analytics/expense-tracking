ce038d4192a103f286f9686f5ef942699e6dafa6
Dev Prakash <dev.prakash.personal21@gmail.com>
2026-07-14 13:05:28 +1000

    Fix GET request code in FastAPI, and add POST request

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
 from datetime import date
+import db_helper
+from typing import List
+from pydantic import BaseModel
 
-app = FastAPI
+class Expense(BaseModel):
+    amount : float
+    category : str
+    notes : str
 
-@app.get("/expenses/{expense_date}")
+app = FastAPI()
+
+@app.get("/expenses/{expense_date}", response_model=List[Expense])
 def get_expenses(expense_date: date):
-    return f"Received get_expense request{date}"
+    expenses = db_helper.fetch_expenses_for_date(expense_date)
+    return expenses
+
+@app.post("/expenses/{expense_date}")
+def add_or_update_expenses(expense_date: date, expenses: List[Expense]):
+    db_helper.delete_expenses_for_date(expense_date)
+    for expense in expenses:
+        db_helper.insert_expense(expense_date, expense.amount, expense.category, expense.notes)
+    return {"message": "Expenses updated successfully"}
