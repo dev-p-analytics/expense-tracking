a133412536acbbeb7bc2c6446ac32376b17d30f6
Dev Prakash <dev.prakash.personal21@gmail.com>
2026-07-15 14:51:30 +1000

    Create and complete backend for analytics tab

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from datetime import date
 import db_helper
 from typing import List
@@ -8,6 +8,10 @@ class Expense(BaseModel):
     amount : float
     category : str
     notes : str
+
+class DateRange(BaseModel):
+    start_date : date
+    end_date : date
 
 app = FastAPI()
 
@@ -22,3 +26,22 @@ def add_or_update_expenses(expense_date: date, expenses: List[Expense]):
     for expense in expenses:
         db_helper.insert_expense(expense_date, expense.amount, expense.category, expense.notes)
     return {"message": "Expenses updated successfully"}
+
+
+@app.post("/analytics/")
+def get_analytics(date_range: DateRange):
+    data = db_helper.fetch_expense_summary(date_range.start_date, date_range.end_date)
+    if data is None:
+        raise HTTPException(status_code = 500, detail = 'Failed to retrieve expense summary from the database')
+
+    total = sum([row['total'] for row in data])
+
+    breakdown = {}
+
+    for row in data: 
+        percentage = (row['total']/total) * 100 if total != 0 else 0
+        breakdown[row['category']] = {
+            'total': row['total'],
+            'percentage': percentage
+        }
+    return breakdown
